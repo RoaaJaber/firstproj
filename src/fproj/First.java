c9644285f00f3c45afc929e1d1ee94fe9c2c1c96
Roaa Jaber <roaa3_1999@hotmail.com>
2020-02-07 22:09:36 +0200

    initial commit

---
+++ src/fproj/First.java
@@ -0,0 +1,11 @@
+package fproj;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to java world");
+
+	}
+
+}
